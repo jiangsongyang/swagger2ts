eb12d50d66cd85b99fb7aee3ef4f99ce400f65a0
jiangsongyang <2744856707@qq.com>
2022-07-28 17:29:35 +0800

    [feat] : add transform target

---
+++ src/core/gen.ts
@@ -0,0 +1,19 @@
+// 最终转换目标
+
+// function getList(id: number, data: DataRequest , options?: any): Promise<ResponseStringResponse> {
+//   const params = {
+//     method: method,
+//     url: basePath + url,
+//     headers: Object.assign({}, requestHeaders, options.headers),
+//     data: data,
+// };
+//   return axios(params).then(response => {
+//       if (response.status >= 200 && response.status < 300) {
+//         return response.data;
+//       }
+//   });
+// }
+
+export const genCode = () => {
+  //
+}
